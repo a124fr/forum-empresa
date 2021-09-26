71b0c2c27f204515d7462850fdedd9287c47d3a6
francisco de assis <a124fr@gmail.com>
2021-09-26 20:50:13 -0300

    trabalhando a ação atualizar topico

---
+++ src/main/java/br/com/empresa/forum/empresa/controller/form/AtuzalizacaoTopicoForm.java
@@ -0,0 +1,47 @@
+package br.com.empresa.forum.empresa.controller.form;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
+import br.com.empresa.forum.empresa.modelo.Topico;
+import br.com.empresa.forum.empresa.repository.TopicoRepository;
+
+public class AtuzalizacaoTopicoForm {
+
+	@NotNull
+	@NotEmpty
+	@Length(min = 5)
+	private String titulo;
+
+	@NotNull
+	@NotEmpty
+	@Length(min = 10)
+	private String mensagem;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public Topico atualizar(Long id, TopicoRepository topicoRepository) {
+		Topico topico = topicoRepository.getById(id);
+		topico.setTitulo(this.titulo);
+		topico.setMensagem(this.mensagem);
+		
+		return topico;
+	}
+
+}
