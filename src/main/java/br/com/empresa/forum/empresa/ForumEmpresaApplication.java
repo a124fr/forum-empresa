ff9f70a9259a01a1c6b1b5dc445524ffb98287b2
francisco de assis <a124fr@gmail.com>
2021-09-27 12:31:12 -0300

    controlando paginação com pageable no parametro do método e habilitando na classe de start da aplicação com a anotação @EnableSpringDataWebSupport

--- src/main/java/br/com/empresa/forum/empresa/ForumEmpresaApplication.java
+++ src/main/java/br/com/empresa/forum/empresa/ForumEmpresaApplication.java
@@ -2,8 +2,10 @@ package br.com.empresa.forum.empresa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class ForumEmpresaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/empresa/forum/empresa/controller/TopicosController.java
+++ src/main/java/br/com/empresa/forum/empresa/controller/TopicosController.java
@@ -1,7 +1,6 @@
 package br.com.empresa.forum.empresa.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -9,10 +8,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,12 +41,25 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 
+//	@GetMapping
+//	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, 
+//			@RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao) {
+//
+//		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
+//				
+//		if (nomeCurso == null) {
+//			Page<Topico> topicos = this.topicoRepository.findAll(paginacao);
+//			return TopicoDTO.converter(topicos);
+//		} else {
+//			Page<Topico> topicos = this.topicoRepository.findByCursoNome(nomeCurso, paginacao);
+//			return TopicoDTO.converter(topicos);
+//		}
+//	}
+	
 	@GetMapping
-	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso, 
-			@RequestParam int pagina, @RequestParam int qtd, @RequestParam String ordenacao) {
-
-		Pageable paginacao = PageRequest.of(pagina, qtd, Direction.DESC, ordenacao);
-				
+	public Page<TopicoDTO> lista(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(sort="id", direction=Direction.DESC, page = 0, size=10) Pageable paginacao) {
+		
 		if (nomeCurso == null) {
 			Page<Topico> topicos = this.topicoRepository.findAll(paginacao);
 			return TopicoDTO.converter(topicos);
