b67fc4fa116dab8f7e223edc7f9ca168fcd39931
francisco de assis <a124fr@gmail.com>
2021-09-27 21:58:18 -0300

    configurando autenticação

--- src/main/java/br/com/empresa/forum/empresa/modelo/Usuario.java
+++ src/main/java/br/com/empresa/forum/empresa/modelo/Usuario.java
@@ -1,20 +1,35 @@
 package br.com.empresa.forum.empresa.modelo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
 @Table(name = "usuarios")
-public class Usuario {
+public class Usuario implements UserDetails {
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
 	private String senha;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<Perfil> perfis = new ArrayList<>(); 
 
 	@Override
 	public int hashCode() {
@@ -71,6 +86,41 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.perfis;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() { // A conta não está expirada?
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {// A conta não está bloqueada?
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {// A credencial não está expirada?
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {// A conta está habilitada?
+		return true;
 	}
 
 }
